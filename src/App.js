a80440580877680ccbe2d2edc8da9055fdc0ec00
Ciwonie <ciwonie@gmail.com>
2018-10-19 00:13:05 -0500

    react file structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import pokemon from "./pokemon.json";
 
 class App extends Component {
+
+  state = {
+    pokemon,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <h1>Hello World</h1>
     );
   }
 }
---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import "./Wrapper.css";
+
+const Wrapper = props => <div className="wrapper">{props.children}</div>;
+
+export default Wrapper;
